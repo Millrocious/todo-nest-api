131cea4d5c3fd6b191127820eabe1371aa895e69
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2024-06-11 20:05:53 +0300

    Add auth and user feature with guards and custom decorators

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'rxjs';
 
-@Controller()
-class AuthController {}
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // return user
+    return this.signToken(user.id, user.email);
+  }
+
+  async signUp(dto: AuthDto) {
+    // gen pass
+    const hash = await argon.hash(dto.password);
+
+    // save user in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+        username: 'user',
+      },
+    });
+
+    // return user
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      userId: userId,
+      email: email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
